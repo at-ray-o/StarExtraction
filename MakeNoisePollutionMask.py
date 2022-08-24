bee9974122cdf312bbe47aa0dc4f41465d67344e
Atreya Dey <13065170+at-ray-o@users.noreply.github.com>
2022-08-24 14:32:31 -0400

    Update MakeNoisePollutionMask.py

--- MakeNoisePollutionMask.py
+++ MakeNoisePollutionMask.py
@@ -10,36 +10,36 @@ fgMask = plt.imread('MaskForAverageImageCropped.png');
 # If overlap with mask dont do it there
 gridSize = 200
 overlap = 50
-xGridIterMax = np.shape(stars)[0]/gridSize;
-yGridIterMax = np.shape(stars)[1]/gridSize;
+xGridIterMax = np.shape(stars)[0]/gridSize
+yGridIterMax = np.shape(stars)[1]/gridSize
 
 starMask = np.ones([np.shape(stars)[0],np.shape(stars)]) # Everything is set to white
 
 #Iterate over all blocks
 for xGridIter in range(0,xGridIterMax)
   for yGridIter in range(0,yGridIterMax)
-    xBeg = (xGridIter)*gridSize;
-    xEnd = (xGridIter)*gridSize+gridSize;
-    yBeg = (yGridIter)*gridSize;
-    yEnd = (yGridIter)*gridSize+gridSize;
+    xBeg = (xGridIter)*gridSize
+    xEnd = (xGridIter)*gridSize+gridSize
+    yBeg = (yGridIter)*gridSize
+    yEnd = (yGridIter)*gridSize+gridSize
 
-    currChan1 = double(stars(xBeg:xEnd,yBeg:yEnd,0));
-    currChan2 = double(stars(xBeg:xEnd,yBeg:yEnd,1));
-    currChan3 = double(stars(xBeg:xEnd,yBeg:yEnd,2));
+    currChan1 = double(stars(xBeg:xEnd,yBeg:yEnd,0))
+    currChan2 = double(stars(xBeg:xEnd,yBeg:yEnd,1))
+    currChan3 = double(stars(xBeg:xEnd,yBeg:yEnd,2))
 
-    blockMeanCh1 = np.mean(currChan1(:));
-    blockMeanCh2 = np.mean(currChan2(:));
-    blockMeanCh3 = np.mean(currChan3(:));
-    blockStdevCh1 = np.std(currChan1(:));
-    blockStdevCh2 = np.std(currChan2(:));
-    blockStdevCh3 = np.std(currChan3(:));
+    blockMeanCh1 = np.mean(currChan1(:))
+    blockMeanCh2 = np.mean(currChan2(:))
+    blockMeanCh3 = np.mean(currChan3(:))
+    blockStdevCh1 = np.std(currChan1(:))
+    blockStdevCh2 = np.std(currChan2(:))
+    blockStdevCh3 = np.std(currChan3(:))
 
-    totalMaskCh1 = np.ones(np.shape(currChan1))*255;
-    totalMaskCh1[(currChan1-blockMeanCh1)>3.0*blockStdevCh1] = 0;
-    totalMaskCh2 = np.ones(np.shape(currChan2));
-    totalMaskCh2[(currChan2-blockMeanCh2)>3.0*blockStdevCh2] = 0;
-    totalMaskCh3 = np.ones(np.shape(currChan3));
-    totalMaskCh3[(currChan3-blockMeanCh3)>3.0*blockStdevCh3] = 0;
+    totalMaskCh1 = np.ones(np.shape(currChan1))*255
+    totalMaskCh1[(currChan1-blockMeanCh1)>3.0*blockStdevCh1] = 0
+    totalMaskCh2 = np.ones(np.shape(currChan2))
+    totalMaskCh2[(currChan2-blockMeanCh2)>3.0*blockStdevCh2] = 0
+    totalMaskCh3 = np.ones(np.shape(currChan3))
+    totalMaskCh3[(currChan3-blockMeanCh3)>3.0*blockStdevCh3] = 0
 
     starMask[xBeg:xEnd,yBeg:yEnd] = np.min(np.min(totalMaskCh1,totalMaskCh2),totalMaskCh3);
 
@@ -53,31 +53,29 @@ xBeg = 1;
 xEnd = size(stars,1);
 yBeg = 1;
 yEnd = size(stars,2);
-currPatchChan1 = double(stars[xBeg:xEnd,yBeg:yEnd,1]);
-currPatchChan2 = double(stars[xBeg:xEnd,yBeg:yEnd,2]);
-currPatchChan3 = double(stars[xBeg:xEnd,yBeg:yEnd,3]);
-currPatchMask = double(starMask[xBeg:xEnd,yBeg:yEnd]);
-currPatchMask = min(currPatchMask,double(fgMask[:,:,1]));
-X = np.zeros(np.shape(currPatchChan1)[0]*np.shape(currPatchChan1)[2]);
-iter = 1;
+currPatchChan1 = double(stars[xBeg:xEnd,yBeg:yEnd,0])
+currPatchChan2 = double(stars[xBeg:xEnd,yBeg:yEnd,1])
+currPatchChan3 = double(stars[xBeg:xEnd,yBeg:yEnd,2])
+currPatchMask = double(starMask[xBeg:xEnd,yBeg:yEnd])
+currPatchMask = min(currPatchMask,double(fgMask[:,:,1]))
+X = np.zeros(np.shape(currPatchChan1)[0]*np.shape(currPatchChan1)[2])
+iter = 1
 for i=xBeg:xEnd
   for j=yBeg:yEnd
-    X(iter,1) = i;
-    X(iter,2) = j;
-    X(iter,3) = 1;
-    iter = iter+1;
-  end
-end
+    X(iter,1) = i
+    X(iter,2) = j
+    X(iter,3) = 1
+    iter = iter+1
 
 
-y = [currPatchChan1(:),currPatchChan2(:),currPatchChan3(:)];
-w = currPatchMask(:);
-beta = lscov(X,y,w);
+y = [currPatchChan1(:),currPatchChan2(:),currPatchChan3(:)]
+w = currPatchMask(:)
+beta = lscov(X,y,w)
 
 currPatchLightPolMask_col = X*beta;
-temp1 = reshape(currPatchLightPolMask_col(:,1),size(currPatchChan1,1),size(currPatchChan1,2));
-temp2 = reshape(currPatchLightPolMask_col(:,2),size(currPatchChan1,1),size(currPatchChan1,2));
-temp3 = reshape(currPatchLightPolMask_col(:,3),size(currPatchChan1,1),size(currPatchChan1,2));
+temp1 = np.reshape(currPatchLightPolMask_col(:,1),np.shape(currPatchChan1)[0],np.shape(currPatchChan1)[1])
+temp2 = np.reshape(currPatchLightPolMask_col(:,2),np.shape(currPatchChan1)[0],np.shape(currPatchChan1)[1])
+temp3 = np.reshape(currPatchLightPolMask_col(:,3),np.shape(currPatchChan1)[0],np.shape(currPatchChan1)[1])
 lightPolMask = zeros(size(stars));
 lightPolMask(:,:,1) = temp1;
 lightPolMask(:,:,2) = temp2;
